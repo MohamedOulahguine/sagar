934fda73be7c64f6d9c6cc840df6c6c710842b8e
mohamedoulahguine <mohamedoulahguine@gmail.com>
2025-01-28 14:53:45 +0300

    Updated header functionality with sticky on scroll

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ let getBody = document.querySelector("body");
 let getTheme = document.querySelector(".theme");
 let getThemeMode = getTheme.querySelectorAll("span");
 let darkMode = localStorage.getItem("darkmode");
-let menuNavOpen = document.querySelector("#menu-nav-open");
-let menuNavClose = document.querySelector("#menu-nav-close");
-let conNav = document.querySelector("nav");
+
+//.......................dark mode
 
 if (darkMode === "enabled") {
   getBody.classList.add("dark-mode");
@@ -29,6 +28,11 @@ getTheme.addEventListener("click", () => {
   });
 });
 
+//....................... menu nav
+let menuNavOpen = document.querySelector("#menu-nav-open");
+let menuNavClose = document.querySelector("#menu-nav-close");
+let conNav = document.querySelector("nav");
+
 menuNavOpen.addEventListener("click", () => {
   document.body.classList.add("no-scroll");
   conNav.style.display = "flex";
@@ -51,5 +55,79 @@ window.addEventListener("resize", () => {
     conNav.style.display = "none";
     menuNavOpen.style.display = "block";
     menuNavClose.style.display = "none";
+  }
+});
+
+//............................ for skills sections
+
+let skillIcons = document.querySelector(".skill-icons");
+
+let iconsLogo = [
+  "icon-skills/icon-javscript.svg",
+  "icon-skills/icon-typescript.svg",
+  "icon-skills/icon-react.svg",
+  "icon-skills/icon-nextjs.svg",
+  "icon-skills/icon-nodejs.svg",
+  "icon-skills/icon-express.svg",
+  "icon-skills/icon-nest.svg",
+  "icon-skills/icon-socket.svg",
+  "icon-skills/icon-postgresql.svg",
+  "icon-skills/icon-mongodb.svg",
+  "icon-skills/icon-sass.svg",
+  "icon-skills/icon-tailwindcss.svg",
+  "icon-skills/icon-figma.svg",
+  "icon-skills/icon-cypress.svg",
+  "icon-skills/icon-storybook.svg",
+  "icon-skills/icon-git.svg",
+];
+let iconsTitle = [
+  "javascript",
+  "Typescript",
+  "react",
+  "next.js",
+  "node.js",
+  "express.js",
+  "nest.js",
+  "socket.io",
+  "postgreSQL",
+  "mongoDB",
+  "sass/scss",
+  "tailwindcss",
+  "figma",
+  "cypress",
+  "storybook",
+  "git",
+];
+
+for (let i = 0; i < iconsLogo.length; i++) {
+  //create elments for skills
+
+  let createContentSkills = document.createElement("div");
+  let createIcons = document.createElement("img");
+  let createIconsTitle = document.createElement("span");
+
+  // give attributes to elements
+
+  createIcons.setAttribute("src", iconsLogo[i]);
+  createIcons.setAttribute("alt", iconsTitle[i]);
+  createContentSkills.className = `${iconsTitle[i]}`;
+  createIconsTitle.innerText = iconsTitle[i];
+
+  // append elememts
+
+  createContentSkills.appendChild(createIcons);
+  createContentSkills.appendChild(createIconsTitle);
+  skillIcons.appendChild(createContentSkills);
+}
+
+//...................Sticky header on scroll
+
+let header = document.getElementById("mainHeader");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 0) {
+    header.style.cssText = "position: fixed; top: 0; z-index: 10000000000;";
+  } else {
+    header.style.cssText = "position: relative; top: 0; z-index: 0;";
   }
 });
